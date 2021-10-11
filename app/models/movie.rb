a4d67fe067baac928423037d037d9d2b4e07e63e
Don Odom <divdonis@gmail.com>
2021-10-10 23:12:25 -0700

    commit tree 03b3cba3525d0bb470ada18d4f8fd88dea5d4fa6

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,38 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+        Movie.new(title).save
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attr)
+        Movie.find_by(attr)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where("(release_date) > 2002")
+    end
+
+    def update_with_attributes(atr)
+        self.update(atr)
+    end
+
+    def self.update_all_titles
+        Movies.update_all
+    end
 end
